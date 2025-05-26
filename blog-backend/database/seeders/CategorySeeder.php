39e1446c5c069a90f3778b62dfe7aa5c44208363
Pyae Phyo Maung <pyaephyomg.ppm06@gmail.com>
2025-05-27 03:02:33 +0700

    Category Dataseed Complete

---
+++ blog-backend/database/seeders/CategorySeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Category;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $categories = [
+            'Politics',
+            'Technology',
+            'Sports',
+            'Health',
+            'Entertainment',
+            'Science',
+            'Business',
+            'World',
+            'Opinion',
+            'Travel',
+        ];
+        foreach ($categories as $category) {
+            Category::create([
+                'name' => $category
+            ]);
+        }
+    }
+}
